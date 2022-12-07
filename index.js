597dc4476c3e15d3a756c3712b8e881fe2e96885
iimofo <61416424+iimofo@users.noreply.github.com>
2022-12-08 01:27:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    const user = req.query.user;
+    res.send(user + "!");
+})
+
+const users = [];
+app.post("/create_user", (req, res) => {
+    const {user} = req.body;
+    users.push({ username: user.username, password: username.password });
+    console.log(users);
+    res.json({loggedIn: true, status: "#OK!"});
+})
+
+app.listen(9000, () => {
+    console.log('Server started on port 5000.');
+})
